a2493eac8866cb52f97898b11f1234b9f936fada
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-19 03:02:44 -0600

    Changing things

--- app/web/codegen.ts
+++ app/web/codegen.ts
@@ -1,17 +1,17 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:5249/graphql',
-  documents: ['src/**/*.{ts,tsx}'],
-  generates: {
-    './src/__generated__/': {
-      preset: 'client',
-      plugins: [],
-      presetConfig: {
-        gqlTagName: 'gql',
-      }
-    }
-  },
+	schema: 'http://localhost:5249/graphql',
+	documents: ['src/**/*.{ts,tsx}'],
+	generates: {
+		'./src/__generated__/': {
+			preset: 'client',
+			plugins: [],
+			presetConfig: {
+				gqlTagName: 'gql',
+			}
+		}
+	},
 };
 
 export default config;
