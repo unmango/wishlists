bea39ceaf7350c3cb1fa577bf11cd74051b73647
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-10-29 22:12:05 -0500

    Static file stuff

--- Dockerfile
+++ Dockerfile
@@ -14,14 +14,26 @@ WORKDIR /build
 #COPY src/UnMango.Wishlists.Api/UnMango.Wishlists.Api.csproj .
 #RUN dotnet restore
 
-COPY src/UnMango.Wishlists.Api/* ./
+COPY src/UnMango.Wishlists.Api/ ./
 
 RUN dotnet publish \
     --use-current-runtime \
     --configuration $CONFIGURATION \
     --output /out
 
+FROM oven/bun:1.3.1-slim AS web
+
+WORKDIR /build
+
+COPY package.json bun.lock ./
+RUN bun install --frozen-lockfile
+
+COPY vite.config.ts tsconfig*.json index.html public/ ./
+COPY src/web/ ./src/web/
+RUN bun run build
+
 FROM mcr.microsoft.com/dotnet/runtime-deps:10.0-noble AS final
 WORKDIR /app
 COPY --from=base /out ./
+COPY --from=web /build/dist ./wwwroot
 ENTRYPOINT ["/app/UnMango.Wishlists.Api"]
--- src/UnMango.Wishlists.Api/Program.cs
+++ src/UnMango.Wishlists.Api/Program.cs
@@ -1,53 +1,31 @@
-using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Http.HttpResults;
-
 var builder = WebApplication.CreateSlimBuilder(args);
 
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-	options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+app.MapOpenApi();
 
-if (app.Environment.IsDevelopment())
-{
-	app.MapOpenApi();
-}
+var api = app.MapGroup("/api");
+var wishlists = api.MapGroup("/wishlists");
+wishlists.MapGet("/", () => Results.Ok(new[] { "Sample Wishlist 1", "Sample Wishlist 2" }));
 
-Todo[] sampleTodos =
-[
-	new(1, "Walk the dog"),
-	new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-	new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-	new(4, "Clean the bathroom"),
-	new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+List<Func<HttpContext, bool>> spaExclusions = [
+	context => !context.Request.Path.StartsWithSegments("/api"),
+	context => !context.Request.Path.StartsWithSegments("/openapi")
 ];
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos)
-	.WithName("GetTodos");
+app.UseWhen(ctx => spaExclusions.Aggregate(true, (acc, fn) => fn(ctx) && acc), then => {
+	if (app.Environment.IsDevelopment()) {
+		then.UseSpa(x => x.UseProxyToSpaDevelopmentServer("http://localhost:5173"));
+	} else {
+		// This isn't handling / for some reason
+		then.UseStaticFiles();
+	}
+});
 
-todosApi.MapGet("/{id}", Results<Ok<Todo>, NotFound> (int id) =>
-		sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-			? TypedResults.Ok(todo)
-			: TypedResults.NotFound())
-	.WithName("GetTodoById");
-
-if (app.Environment.IsDevelopment())
-{
-	// app.Map("/", x => x.UseSpa(y => y.UseProxyToSpaDevelopmentServer("http://localhost:5173")));
-	app.UseSpa(x => x.UseProxyToSpaDevelopmentServer("http://localhost:5173"));
+// This just doesn't work
+if (app.Environment.IsProduction()) {
+	app.MapStaticAssets();
 }
 
 app.Run();
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-}
