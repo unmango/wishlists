69600ccb08fe8197b9e7544e5c1feed2b402b2cb
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-11-02 20:59:24 -0600

    Configures the wishlist entity.
    Configures the wishlist entity for the database context,
    including setting the primary key, name property, and owner
    relationship. This ensures proper schema definition and
    data integrity when interacting with wishlists.

--- src/UnMango.Wishlists.Api/Domain/AppDbContext.cs
+++ src/UnMango.Wishlists.Api/Domain/AppDbContext.cs
@@ -7,8 +7,15 @@ namespace UnMango.Wishlists.Api.Domain;
 internal sealed class AppDbContext(DbContextOptions<AppDbContext> options)
 	: IdentityDbContext<User, IdentityRole<Guid>, Guid>(options)
 {
+	public DbSet<Wishlist> Wishlists { get; init; }
+
 	protected override void OnModelCreating(ModelBuilder modelBuilder) {
 		base.OnModelCreating(modelBuilder);
+
+		var wishlist = modelBuilder.Entity<Wishlist>();
+		wishlist.HasKey(x => x.Id);
+		wishlist.Property(x => x.Name).IsRequired();
+		wishlist.HasOne(x => x.Owner);
 	}
 
 	public static void Configure(IServiceProvider services, DbContextOptionsBuilder options) {
