ac2cb3879aadfa730b6c0b836761122e3b5c61bb
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-11-07 12:25:07 -0600

    WIP

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           buildInputs = with pkgs; [
             git
             gnumake
-						docker
+            docker
             nixfmt-rfc-style
             nixfmt-tree
             shellcheck
