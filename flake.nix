68c244544934866d99b19b7c76aa3b9ef5c6c61b
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-11-23 12:09:42 -0600

    WIP

--- flake.nix
+++ flake.nix
@@ -71,6 +71,11 @@
             meta.description = "Wishlists Application";
           };
 
+          apps.cr = {
+            type = "app";
+            program = "${chart-releaser}/bin/cr";
+          };
+
           devShells.default = pkgs.callPackage ./shell.nix {
             inherit pkgs;
             bun2nix = inputs.bun2nix.packages.${system}.default;
