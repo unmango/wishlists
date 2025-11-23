ed3ea7c43aaa054dc06f32166d9c48216b7f6561
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-11-23 11:08:33 -0600

    Chart testing

--- shell.nix
+++ shell.nix
@@ -4,22 +4,24 @@
 }:
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
+    bun
+    bun2nix
+    chart-testing
+    docker
+    dotnetCorePackages.sdk_10_0
     gh
     git
     gnumake
-    docker
+    nixd
     nixfmt-rfc-style
+    nodejs # For JetBrains tools
     shellcheck
     watchexec
-    dotnetCorePackages.sdk_10_0
-    nodejs # For JetBrains tools
-    bun
-    bun2nix
-    nixd
   ];
 
   BUN = pkgs.bun + "/bin/bun";
   BUN2NIX = bun2nix + "/bin/bun2nix";
+  CT = pkgs.chart-testing + "/bin/ct";
   DOTNET = pkgs.dotnetCorePackages.sdk_10_0 + "/bin/dotnet";
   NIXFMT = pkgs.nixfmt-rfc-style + "/bin/nixfmt";
   NODE = pkgs.nodejs + "/bin/node";
