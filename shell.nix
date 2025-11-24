3b8961bd2de2125e9af72093a8bcf0e7a02f1edd
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-11-23 23:41:44 -0600

    WIP

--- shell.nix
+++ shell.nix
@@ -12,6 +12,7 @@ pkgs.mkShell {
     gh
     git
     gnumake
+    kubernetes-helm
     nixd
     nixfmt-rfc-style
     nodejs # For JetBrains tools
@@ -23,6 +24,7 @@ pkgs.mkShell {
   BUN2NIX = bun2nix + "/bin/bun2nix";
   CT = pkgs.chart-testing + "/bin/ct";
   DOTNET = pkgs.dotnetCorePackages.sdk_10_0 + "/bin/dotnet";
+  HELM = pkgs.kubernetes-helm + "/bin/helm";
   NIXFMT = pkgs.nixfmt-rfc-style + "/bin/nixfmt";
   NODE = pkgs.nodejs + "/bin/node";
 }
