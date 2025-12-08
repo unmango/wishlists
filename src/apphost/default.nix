bdb6cd5c7ecef0ddbdee56b00ad34a0cc14d7c9c
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-12-07 22:14:39 -0600

    apphost stuff

---
+++ src/apphost/default.nix
@@ -0,0 +1,27 @@
+{
+  perSystem =
+    { pkgs, lib, ... }:
+    let
+      sdk = pkgs.dotnetCorePackages.sdk_10_0_1xx;
+      version = "0.0.1";
+      apphost = pkgs.buildDotnetModule {
+        inherit version;
+
+        pname = "apphost";
+        src = lib.cleanSource ./.;
+        projectFile = ./apphost.csproj;
+        dotnet-sdk = sdk;
+
+        meta = with lib; {
+          homepage = "https://github.com/unmango/wishlists";
+          description = "UnMango Wishlists AppHost";
+          license = licenses.mit;
+          maintainers = [ maintainers.UnstoppableMango ];
+        };
+      };
+    in
+    {
+      packages.apphost = apphost;
+      legacyPackages.apphost = apphost;
+    };
+}
